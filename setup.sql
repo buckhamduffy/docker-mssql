fd5b966dab1e349c5bae4563625acb11a056ef17
Aaron Florey <azza@jcaks.net>
2021-11-17 11:00:42 +1000

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,9 +1,12 @@
 /*
 Enter custom T-SQL here that would run after SQL Server has started up.
 */
-
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'testing_db')
 BEGIN
 CREATE DATABASE testing_db;
 END;
-GO
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'aps_testing_db')
+BEGIN
+CREATE DATABASE aps_testing_db;
+END;
